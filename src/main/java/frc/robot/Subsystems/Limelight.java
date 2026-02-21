5510c4879ccfba4665fe2149c6456a2be0df2425
Zachariah Ugochuku <zachugochuku@gmail.com>
2026-02-21 14:06:27 -0500

    Clarified deltaX; added tag identification to limelight

--- src/main/java/frc/robot/Subsystems/Limelight.java
+++ src/main/java/frc/robot/Subsystems/Limelight.java
@@ -4,9 +4,11 @@ import frc.robot.LimelightHelpers;
 
 public class Limelight {
     private final String limelightName;
+    private final LimelightHelpers.RawFiducial[] fiducials;
 
     public Limelight(String name) {
         limelightName = name;
+        fiducials = LimelightHelpers.getRawFiducials(limelightName);
     }
     
     public double getDeltaX(double targetHeight, double camHeight, double camAngle) {
@@ -18,6 +20,17 @@ public class Limelight {
         }
         // if the limelight doesn't have a valid target, return 0, causing the shooter to stop rather than throw an exception.
         return 0;
-    } 
+    }
+    
+    public LimelightHelpers.RawFiducial identifyyTag(int tagID) {
+        if (fiducials.length > 0) {
+            for (LimelightHelpers.RawFiducial fiducial : fiducials) {
+                if (fiducial.id == tagID) {
+                    return fiducial;
+                }
+            }
+        }
+        return null;
+    }
 }
 
