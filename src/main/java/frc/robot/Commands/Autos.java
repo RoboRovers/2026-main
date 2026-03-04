e69295c7ac749d6cbf5f125bcd0068322b9c2d62
TheKerbalGuy615 <denlingerc@roverkids.org>
2026-03-04 16:37:47 -0500

    Even even more error fixes

--- src/main/java/frc/robot/Commands/Autos.java
+++ src/main/java/frc/robot/Commands/Autos.java
@@ -23,40 +23,34 @@ import frc.robot.Util.Constants.Constants_AprilTags;
 import frc.robot.Util.Constants.Constants_Auto;
 
 public final class Autos {
-  private Swerve s_Swerve;
-  //private Vision s_Vision;//TODO: Add vision subsystem
-  private RobotConfig config;
-  private PIDController translationConstants = new PIDController(Constants_Auto.P_TRANSLATION, Constants_Auto.I_TRANSLATION, Constants_Auto.D_TRANSLATION);
-  private PIDController rotationConstants = new PIDController(Constants_Auto.P_THETA, Constants_Auto.I_THETA, Constants_Auto.D_THETA);
   
-  public Autos(Drive c_Drive, Swerve s_Swerve, Shooter s_Shooter, Intake s_Intake/* , Vision s_Vision*/, RobotConfig config) {
-    this.s_Swerve = s_Swerve;
-    //this.s_Vision = s_Vision;//TODO: Add vision subsystem
- 
-
-    try {
-      config = RobotConfig.fromGUISettings();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  
-    AutoBuilder.configure(
-      s_Swerve::getPose,
-      s_Swerve::resetOdometry,
-      s_Swerve::getRobotRelativeSpeeds,
-      s_Swerve::setModuleStates,
-      pathController,
-      config,
-      s_Swerve::allianceCheck,
-      s_Swerve);
-
-
-      NamedCommands.registerCommand("Face Forward Wheels", Commands.runOnce(() -> s_Swerve.faceAllForward())); //Should be ran at the start of every auto.
-      }
+  private PIDController translationConstants;
+    private PIDController rotationConstants;
+    
+      public Autos(Drive c_Drive, Swerve s_Swerve, Shooter s_Shooter, Intake s_Intake/* , Vision s_Vision*/, RobotConfig config) {
+        try {
+          config = RobotConfig.fromGUISettings();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
       
-    public PathFollowingController pathController = new PPHolonomicDriveController(
-      new com.pathplanner.lib.config.PIDConstants(translationConstants.getP(), translationConstants.getI(), translationConstants.getD()),
-      new com.pathplanner.lib.config.PIDConstants(rotationConstants.getP(), rotationConstants.getI(), rotationConstants.getD()));
+        AutoBuilder.configure(
+          s_Swerve::getPose,
+          s_Swerve::resetOdometry,
+          s_Swerve::getRobotRelativeSpeeds,
+          s_Swerve::setModuleStates,
+          pathController,
+          config,
+          s_Swerve::allianceCheck,
+          s_Swerve);
+    
+    
+          NamedCommands.registerCommand("Face Forward Wheels", Commands.runOnce(() -> s_Swerve.faceAllForward())); //Should be ran at the start of every auto.
+          }
+          
+        public PathFollowingController pathController = new PPHolonomicDriveController(
+          new com.pathplanner.lib.config.PIDConstants(translationConstants.getP(), translationConstants.getI(), translationConstants.getD()),
+        new com.pathplanner.lib.config.PIDConstants(rotationConstants.getP(), rotationConstants.getI(), rotationConstants.getD()));
 
 }
 
